6d393e2bb2185524b1a72d260b585293bd8eaa56
rsvn <rizwanshaz2004@gmail.com>
2025-08-24 23:45:33 +0530

    portfolio img's

--- script.js
+++ script.js
@@ -27,3 +27,53 @@ document.addEventListener('DOMContentLoaded', () => {
       });
     });
   });
+
+  
+
+
+
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  const modal = document.getElementById("imageModal");
+  const modalImg = document.getElementById("modalImg");
+  const gallery = document.getElementById("gallery");
+  const totalImages = 8;
+  const folder = "asssets/images/";
+
+  for (let i = 1; i <= totalImages; i++) {
+    let img = document.createElement("img");
+    img.src = `${folder}img${i}.jpg`;
+    img.alt = `Image ${i}`;
+
+    let div = document.createElement("div");
+    div.className = "gallery-item";
+    div.appendChild(img);
+
+    div.addEventListener("click", () => {
+       modal.classList.add("show");
+      modal.style.display = "flex";
+      modalImg.src = img.src;
+        modal.style.background = "rgba(0,0,0,0.7)";
+    });
+
+    gallery.appendChild(div);
+  }
+
+  // Close when clicking outside image
+  modal.addEventListener("click", (e) => {
+    if (e.target === modal) {
+      modal.style.display = "none";
+        modal.style.background = "none";
+    }
+  });
+
+  // Close with Esc key
+  document.addEventListener("keydown", (e) => {
+    if (e.key === "Escape") {
+      modal.style.display = "none";
+        modal.style.background = "none";
+    }
+  });
+});
+
